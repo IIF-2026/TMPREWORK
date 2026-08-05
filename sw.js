30c7e9644b9393ef7a03b21d2b3ce739ae476d7f
IIF-2026 <nagesh@inqui-lab.org>
2026-08-05 17:55:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tm-prework-v1';
+const CACHE='tm-prework-v2';
 const CORE=['./','./index.html','./course-data.js','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
