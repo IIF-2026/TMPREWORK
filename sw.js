4dbb3881b3e17a482972463426a7d533900233e8
IIF-2026 <nagesh@inqui-lab.org>
2026-08-10 19:01:48 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tm-prework-v13';
+const CACHE='tm-prework-v15';
 const CORE=['./','./index.html','./course-data.js','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(
